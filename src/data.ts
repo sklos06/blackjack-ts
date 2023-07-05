7d7731c6dadfbbb42a51c5771d371cad3c0f8008
Szymon <sklos0620@gmail.com>
2023-07-06 01:21:04 +0200

    added cards and data

---
+++ src/data.ts
@@ -0,0 +1,269 @@
+type Card = {
+    id: number;
+    url: string;
+    value: number;
+};
+
+const data:Card[] = [
+    {
+        id:0,
+        url: "./Cards/ace-club.png",
+        value: 11
+    },
+    {
+        id:1,
+        url: "./Cards/ace-diamond.png",
+        value: 11
+    },
+    {
+        id:2,
+        url: "./Cards/ace-heart.png",
+        value: 11
+    },
+    {
+        id:3,
+        url: "./Cards/ace-spade.png",
+        value: 11
+    },
+    {
+        id:4,
+        url: "./Cards/two-club.png",
+        value: 2
+    },
+    {
+        id:5,
+        url: "./Cards/two-diamond.png",
+        value: 2
+    },
+    {
+        id:6,
+        url: "./Cards/two-heart.png",
+        value: 2
+    },
+    {
+        id:7,
+        url: "./Cards/two-spade.png",
+        value: 2
+    },
+    {
+        id:8,
+        url: "./Cards/three-club.png",
+        value: 3
+    },
+    {
+        id:9,
+        url: "./Cards/three-diamond.png",
+        value: 3
+    },
+    {
+        id:10,
+        url: "./Cards/three-heart.png",
+        value: 3
+    },
+    {
+        id:11,
+        url: "./Cards/three-spade.png",
+        value: 3
+    },
+    {
+        id:12,
+        url: "./Cards/four-club.png",
+        value: 4
+    },
+    {
+        id:13,
+        url: "./Cards/four-diamond.png",
+        value: 4
+    },
+    {
+        id:14,
+        url: "./Cards/four-heart.png",
+        value: 4
+    },
+    {
+        id:15,
+        url: "./Cards/four-spade.png",
+        value: 4
+    },
+    {
+        id:16,
+        url: "./Cards/five-club.png",
+        value: 5
+    },
+    {
+        id:17,
+        url: "./Cards/five-diamond.png",
+        value: 5
+    },
+    {
+        id:18,
+        url: "./Cards/five-heart.png",
+        value: 5
+    },
+    {
+        id:19,
+        url: "./Cards/five-spade.png",
+        value: 5
+    },
+    {
+        id:20,
+        url: "./Cards/six-club.png",
+        value: 6
+    },
+    {
+        id:21,
+        url: "./Cards/six-diamond.png",
+        value: 6
+    },
+    {
+        id:22,
+        url: "./Cards/six-heart.png",
+        value: 6
+    },
+    {
+        id:23,
+        url: "./Cards/six-spade.png",
+        value: 6
+    },
+    {
+        id:24,
+        url: "./Cards/seven-club.png",
+        value: 7
+    },
+    {
+        id:25,
+        url: "./Cards/seven-diamond.png",
+        value: 7
+    },
+    {
+        id:26,
+        url: "./Cards/seven-heart.png",
+        value: 7
+    },
+    {
+        id:27,
+        url: "./Cards/seven-spade.png",
+        value: 7
+    },
+    {
+        id:28,
+        url: "./Cards/eight-club.png",
+        value: 8
+    },
+    {
+        id:29,
+        url: "./Cards/eight-diamond.png",
+        value: 8
+    },
+    {
+        id:30,
+        url: "./Cards/eight-heart.png",
+        value: 8
+    },
+    {
+        id:31,
+        url: "./Cards/eight-spade.png",
+        value: 8
+    },
+    {
+        id:32,
+        url: "./Cards/nine-club.png",
+        value: 9
+    },
+    {
+        id:33,
+        url: "./Cards/nine-diamond.png",
+        value: 9
+    },
+    {
+        id:34,
+        url: "./Cards/nine-heart.png",
+        value: 9
+    },
+    {
+        id:35,
+        url: "./Cards/nine-spade.png",
+        value: 9
+    },
+    {
+        id:36,
+        url: "./Cards/ten-club.png",
+        value: 10
+    },
+    {
+        id:37,
+        url: "./Cards/ten-diamond.png",
+        value: 10
+    },
+    {
+        id:38,
+        url: "./Cards/ten-heart.png",
+        value: 10
+    },
+    {
+        id:39,
+        url: "./Cards/ten-spade.png",
+        value: 10
+    },
+    {
+        id:40,
+        url: "./Cards/jack-club.png",
+        value: 10
+    },
+    {
+        id:41,
+        url: "./Cards/jack-diamond.png",
+        value: 10
+    },
+    {
+        id:42,
+        url: "./Cards/jack-heart.png",
+        value: 10
+    },
+    {
+        id:43,
+        url: "./Cards/jack-spade.png",
+        value: 10
+    },
+    {
+        id:44,
+        url: "./Cards/queen-club.png",
+        value: 10
+    },
+    {
+        id:45,
+        url: "./Cards/queen-diamond.png",
+        value: 10
+    },
+    {
+        id:46,
+        url: "./Cards/queen-heart.png",
+        value: 10
+    },
+    {
+        id:47,
+        url: "./Cards/queen-spade.png",
+        value: 10
+    },
+    {
+        id:48,
+        url: "./Cards/king-club.png",
+        value: 10
+    },
+    {
+        id:49,
+        url: "./Cards/king-diamond.png",
+        value: 10
+    },
+    {
+        id:50,
+        url: "./Cards/king-heart.png",
+        value: 10
+    },
+    {
+        id:51,
+        url: "./Cards/king-spade.png",
+        value: 10
+    }
+]
+export default data
